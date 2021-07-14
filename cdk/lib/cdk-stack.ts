f5d046dd6dec8782bec763fc9126ac2f27565138
natalia <53310862+ripleycmd@users.noreply.github.com>
2021-07-14 14:43:31 +0100

    Add cloudfront.

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,23 +1,25 @@
-import * as cdk from '@aws-cdk/core';
-import * as iam from '@aws-cdk/aws-iam'
-import * as s3 from '@aws-cdk/aws-s3'
-import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'nm-static-react-hosting', {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess:true,
-      removalPolicy:cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
+    const bucket = new s3.Bucket(this, "nm-static-react-hosting", {
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
     new s3Deployment.BucketDeployment(this, "nm-static-react-deployment", {
       destinationBucket: bucket,
       sources: [s3Deployment.Source.asset("../build")],
-    })
+    });
 
     const boundary = iam.ManagedPolicy.fromManagedPolicyArn(
       this,
@@ -27,8 +29,20 @@ export class CdkStack extends cdk.Stack {
     iam.PermissionsBoundary.of(this).apply(boundary);
 
 
-    new cdk.CfnOutput(this, 'Bucket-URL', {
-      value: bucket.bucketDomainName
-    })
+    const distribution = new cloudfront.Distribution(
+      this,
+      "nm-static-react-distribution",
+      {
+        defaultBehavior: {origin: new origins.S3Origin(bucket)}
+      }
+    );
+
+    new cdk.CfnOutput(this, "Bucket-URL", {
+      value: bucket.bucketDomainName,
+    });
+
+    new cdk.CfnOutput(this, "Cloudfront-URL", {
+      value: distribution.distributionDomainName,
+    });
   }
 }
